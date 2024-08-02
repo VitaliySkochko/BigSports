ad5bf1ff8164c968ad6f59eef178e57dc7973e39
VitaliySkochko <vitaliyskochko@i.ua>
2024-08-02 12:48:40 +0300

    Update firebase configuration

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAj7qkr-RW4RrzZy4WHNEd9Ps0g4ovZRwk",
-  authDomain: "ukrainianfootball-4a1fb.firebaseapp.com",
-  projectId: "ukrainianfootball-4a1fb",
-  storageBucket: "ukrainianfootball-4a1fb.appspot.com",
-  messagingSenderId: "662439826757",
-  appId: "1:662439826757:web:1400a85bbcbf20370e1a2c"
+  apiKey: "AIzaSyCb0ZbLfcPyWP1LPfmrbGFLcqjS0rkPHUU",
+  authDomain: "bigsports-a911c.firebaseapp.com",
+  projectId: "bigsports-a911c",
+  storageBucket: "bigsports-a911c.appspot.com",
+  messagingSenderId: "107030355894",
+  appId: "1:107030355894:web:df78f046bd88dbb466eb17",
+  measurementId: "G-QV0VFRQ0ZJ"
 };
 
 const app = initializeApp(firebaseConfig);
