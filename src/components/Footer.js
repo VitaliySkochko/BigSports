c22c9f0b95f4f26a469ad2fa53aa9458291336f4
VitaliySkochko <vitaliyskochko@i.ua>
2024-08-16 13:34:34 +0300

    Доробив Футер

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,31 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../img/logo.png'; 
 
 const Footer = () => {
   return (
     <footer className="footer">
-      <p>&copy; {new Date().getFullYear()} Українські футбольні новини. Всі права захищені.</p>
+      <div className="footer-content">
+        <div className="footer-logo">
+          <img src={logo} alt="BigSports Logo" />
+        </div>
+        <div className="footer-links">
+          <Link to="/football-ukraine">Футбол України</Link>
+          <Link to="/world-football">Світовий футбол</Link>
+          <Link to="/boxing">Бокс</Link>
+          <Link to="/tennis">Теніс</Link>
+          <Link to="/biathlon">Біатлон</Link>
+        </div>
+        <div className="footer-contact">
+          <p>Пишіть нам: bigsportssait@gmail.com</p>
+          <p>Всі права на матеріали, що знаходяться на сайті охороняються відповідно до законодавства України.</p>
+          <p>&copy; BigSports {new Date().getFullYear()}</p>
+        </div>
+      </div>
     </footer>
   );
 };
 
 export default Footer;
+
+
